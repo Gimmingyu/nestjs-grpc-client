f4f76fa13484a52ff61e5377dc6db3265174acd4
김민규 <gimmingyu@gimmingyuui-iMac.local>
2023-02-15 20:13:49 +0900

    add(dir): Add proto dir

--- src/domain/user/interface/user.interface.ts
+++ src/domain/user/interface/user.interface.ts
@@ -2,8 +2,6 @@ import { Observable } from 'rxjs';
 
 export interface UserService {
   GreetUser(data: WelcomeUserRequest): Observable<WelcomeUserResponse>;
-
-  StreamGreet(data: StreamGreetRequest): Observable<StreamGreetResponse>;
 }
 
 export interface WelcomeUserRequest {
@@ -12,12 +10,4 @@ export interface WelcomeUserRequest {
 
 export interface WelcomeUserResponse {
   greet: string;
-}
-
-export interface StreamGreetRequest {
-  msg: string;
-}
-
-export interface StreamGreetResponse {
-  msg: string;
 }
